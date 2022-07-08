ca4a8fecc09fbca50b47b48ba8d5653905736dcf
sekachev <sekachev@proactor.pro>
2022-07-08 09:16:07 +0300

    register_blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,27 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from app.errors import bp as errors_bp
+from app.auth import bp as auth_bp
 from config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-login = LoginManager(app)
+app.register_blueprint(errors_bp)
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+login = LoginManager()
 login.login_view = 'login'
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app import routes, models,  errors
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
+from app import routes, models
